0f3475d74cb24f005ea6f2e3597ba28901d73877
Aglool <96567129+Aglool@users.noreply.github.com>
2023-03-14 22:37:22 +0300

    Add files via upload
    Среднее арифметическое двух чисел

---
+++ Homework_cs/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_cs
+{
+    class Program
+    {
+        //среднее арифмитическое двух чисел
+        static void Main(string[] args)
+        {
+            Console.Write("Введите первое число: ");
+            double a1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите второе число: ");
+            double b1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Среднее арифметическое: ");
+            double d = (a1 + b1) / 2;
+            Console.WriteLine(d);
+        }
+    }
+}
