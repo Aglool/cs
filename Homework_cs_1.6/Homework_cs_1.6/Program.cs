8e966e5250b33f9854c5f493ad1b6d788c4e22fc
Aglool <96567129+Aglool@users.noreply.github.com>
2023-03-14 22:45:17 +0300

    Add files via upload
    Подсчёт количества чётных и нечётных чисел в том диапазоне, который укажет пользователь

---
+++ Homework_cs_1.6/Homework_cs_1.6/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_cs_1._6
+{
+    class Program
+    {
+        //подсчёт кол-ва четных и нечетных чисел в том диапазоне, который укажет пользователь
+        static void Main(string[] args)
+        {
+            int count = 0;
+            int count2 = 0;
+            int num = 0;
+            Console.Write("Введите диапазон: ");
+            int limit = int.Parse(Console.ReadLine());
+            while (num < limit)
+            {
+                if (num % 2 == 0)
+                {
+                    count += 1;
+                }
+                else
+                {
+                    count2 += 1;
+                }
+                num++;
+            }
+            Console.WriteLine("Количество четных чисел: " + count);
+            Console.WriteLine("Количество нечетных чисел: " + count2);
+            Console.ReadLine();
+        }
+    }
+}
