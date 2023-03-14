d600918b4fc24aee48b11a62f8e0054280555ccc
Aglool <96567129+Aglool@users.noreply.github.com>
2023-03-14 22:41:39 +0300

    Add files via upload
    Простейший конвертер валют. Валюта задана хардкодом и не изменяется

---
+++ Homework_cs_1.3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_cs_1._3
+{
+    class Program
+    {
+        //простой конвертер валют (доллары в рубли)
+        static void Main(string[] args)
+        {
+            double a = 60.78;
+
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            double c = a * b;
+            Console.WriteLine(c + " рублей в " + b + " долларах");
+        }
+    }
+}
