9501eb443f876b74f16099f9e74a6b26ce4bee42
Aglool <96567129+Aglool@users.noreply.github.com>
2023-03-14 22:39:35 +0300

    Add files via upload
    Вывести на экран сумму и произведение трёх чисел, заданных пользователем

---
+++ Homework_cs_1.2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_cs_1._2
+{
+    class Program
+    {
+        //ввести три числа и вывести на экран их сумму и произведение
+        static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            double c = double.Parse(Console.ReadLine());
+
+            double p = a * b * c;
+            double s = a + b + c;
+
+            Console.WriteLine("a * b * c = " + p);
+            Console.WriteLine("a + b + c = " + s);
+        }
+    }
+}
