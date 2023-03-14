c7d049dea363042da1fd31024fbfa75b5deebd9f
Aglool <96567129+Aglool@users.noreply.github.com>
2023-03-14 22:42:50 +0300

    Add files via upload
    Проверка введённого с клавиатуры числа на чётность

---
+++ Homework_cs_1.4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_cs_1._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число для проверки: ");
+            int num = int.Parse(Console.ReadLine());
+
+
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("Число четное");
+            }
+            else
+            {
+                Console.WriteLine("Число нечетное");
+            }
+            Console.ReadLine();
+        }
+    }
+}
